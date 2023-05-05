cfc8217c75669dc80659c7edc0c50d9a6845eb13
Robin <robinbrunel8@gmail>
2023-05-05 15:50:26 +0200

    ajout dossier Modif Dev et test Robin

---
+++ TestCinema dev/Cinema_Spring/Cinema_back/src/test/java/fr/inti/SalleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
